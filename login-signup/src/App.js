a8e1d114dbd784790e1372a78b08829ed20376bb
vineeta-chauhan <vineetachauhan69@gmail.com>
2021-05-22 13:38:53 +0530

    used the useffect and cleanup

--- login-signup/src/App.js
+++ login-signup/src/App.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './Components/Login/Login';
-import Home from './Components/Home/Home';
-import MainHeader from './Components/MainHeader/MainHeader';
+import Login from "./Components/Login/Login";
+import Home from "./Components/Home/Home";
+import MainHeader from "./Components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn','1')
     setIsLoggedIn(true);
   };
 
